a3d671062eccb977880538e6f2f9bd39ea4e09e4
Viktor Malyshev <vmalyshev0@gmail.com>
2023-10-16 12:43:15 +0200

    next greater digit

--- app/src/main/java/puzzlers/Puzzlers.java
+++ app/src/main/java/puzzlers/Puzzlers.java
@@ -1,7 +1,9 @@
 package puzzlers;
 
 import puzzlers.tasks.FindWordsPuzzle;
+import puzzlers.tasks.NextElemSameDigits;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,14 +11,14 @@ public class Puzzlers {
 
     public static void main(String[] args) {
         char[][] puzzle = {
-                            {'F','Y','Y','H','N','R','D'},
+                {'F','Y','Y','H','N','R','D'},
                             {'R','L','J','C','I','N','U'},
                             {'A','A','W','A','A','H','R'},
                             {'N','T','K','L','P','N','E'},
                             {'C','I','L','F','S','A','P'},
                             {'E','O','G','O','T','P','N'},
                             {'H','P','O','L','A','N','D'}
-                          };
+        };
         Set<String> words = new HashSet<>();
         words.add("FRANCE");
         words.add("POLAND");
@@ -26,5 +28,10 @@ public class Puzzlers {
         words.add("HOLLAND");
         Set<String> wordsFound = FindWordsPuzzle.findWords(puzzle, words);
         wordsFound.forEach(System.out::println);
+
+        NextElemSameDigits.findNextGreater(new int[]{6});
+        NextElemSameDigits.findNextGreater(new int[]{1, 2, 3, 4});
+        NextElemSameDigits.findNextGreater(new int[]{1, 2, 3, 2});
+        NextElemSameDigits.findNextGreater(new int[]{6, 2, 1, 8, 7, 3});
     }
 }
