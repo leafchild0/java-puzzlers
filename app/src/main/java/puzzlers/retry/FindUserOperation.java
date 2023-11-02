f012793d5106b019499d72856ae52aa68646ea02
Viktor Malyshev <vmalyshev0@gmail.com>
2023-11-02 14:29:10 +0100

    small refactoring 2

--- app/src/main/java/puzzlers/retry/FindUserOperation.java
+++ app/src/main/java/puzzlers/retry/FindUserOperation.java
@@ -9,8 +9,8 @@ import java.util.List;
  * @param userId - id of user to find
  */
 public record FindUserOperation(String userId, List<OperationException> errors) implements Operation<String> {
-        public FindUserOperation(String customerId, OperationException... errors) {
-            this(customerId, new ArrayList<>(List.of(errors)));
+        public FindUserOperation(String userId, OperationException... errors) {
+            this(userId, new ArrayList<>(List.of(errors)));
         }
         @Override
         public String perform() throws OperationException {
